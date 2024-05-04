e8092c0d559a6d0b32c269dcce066d71de7e3e06
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2024-05-04 20:31:30 +0800

    feat: add shadcn as the component library
    Co-authored-by: Malik <Malixl@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/ui/Navbar.vue";
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Navbar />
+  <section>
+    <div class="container flex justify-between items-center">
+      <div class="flex flex-col gap-4">
+        <h1 class="text-3xl font-bold">
+          Web<span class="text-yellow-600">GIS.</span> Destana
+        </h1>
+        <p class="text-xl">
+          Website Sistem Informasi Geografis Desa Tangguh Bencana
+        </p>
+        <div class="flex gap-4">
+          <Button>Lihat Peta</Button>
+          <Button variant="outline">Kontak Darurat</Button>
+        </div>
+      </div>
+      <img class="basis-1/2" width="200" src="/img/heroimage.png" alt="" />
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
+import path from "path";
+import tailwind from "tailwindcss";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
