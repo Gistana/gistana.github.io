fb3a7caf96123edda85af3a77e385d72c225027d
Yasdil Lasoma <yasdilofficial@gmail.com>
2024-05-27 23:16:38 +0800

    tambah modal

--- src/views/map.vue
+++ src/views/map.vue
@@ -41,9 +41,11 @@ export default {
   },
   data() {
     return {
-      center: [0.36694401562217005, 123.29020767212],
+      center: [0.3702477, 123.2836842],
       administrasi,
       currentLocation: null,
+      dataModal:null,
+      modal:false,
 
       jalurEvakuasiMamungaaTimur,
       titikKumpulMamungaaTimur,
@@ -91,11 +93,44 @@ export default {
         features: [feature],
       }));
     },
+    showModal(index) {
+      this.dataModal = index.layer.feature.properties
+      console.log(index.layer.feature.properties);
+      this.modal = true
+    },
+    closeModal() {
+      this.dataModal = []
+      this.modal = false
+    }
   },
 };
 </script>
 
 <template>
+  <div
+    v-if="modal"
+    class="fixed inset-0 flex items-end justify-end bg-opacity-50 m-5"
+    style="z-index:9999"
+  >
+    <div class="bg-white rounded-lg shadow-lg w-1/3" >
+      <div class="flex justify-between items-center border-b p-4">
+        <h3 class="text-xl font-semibold">{{dataModal.Name ?? 'Batas Administrasi'}}</h3>
+        <button @click="closeModal" class="text-gray-500 hover:text-gray-700">&times;</button>
+      </div>
+      <div class="p-4">
+        <table class="table min-w-full bg-white" style="border-radius:15px;">
+            <tr v-for="(item,key) in dataModal" style="border: 1px solid #fff" class="p-3">
+                <th style="width:200px" class="bg-yellow-700 text-white p-1">{{key}}</th>
+                <td class="bg-yellow-700 text-white p-1">:</td>
+                <td class="text-yellow-600 bg-gray-100 p-1">{{item}}</td>
+            </tr>
+        </table>
+      </div>
+      <div class="flex justify-end border-t p-4">
+        <button class="bg-gray-300 text-gray-700 px-4 py-2 rounded mr-2" @click="closeModal">Close</button>
+      </div>
+    </div>
+  </div>
   <div class="flex h-svh flex-row">
     <sidebar>
       <div class="flex flex-col gap-2">
@@ -175,8 +210,8 @@ export default {
       class="isolate ms-3"
       :center="center"
       :use-global-leaflet="false"
-      :zoom="12"
-      :min-zoom="12"
+      :zoom="13"
+      :min-zoom="13"
       ref="map"
     >
       <l-control-layers position="topright" />
@@ -220,6 +255,7 @@ export default {
         v-for="geoJSON in separateFeatures(administrasi)"
         :geojson="geoJSON"
         :optionsStyle="() => ({ color: geoJSON.features[0].properties.COLOR })"
+        @click="showModal"
       >
         <l-tooltip>{{ geoJSON.name }}</l-tooltip>
       </l-geo-json>
@@ -228,37 +264,50 @@ export default {
         v-if="showJalurEvakuasiMamungaaTimur"
         :geojson="jalurEvakuasiMamungaaTimur"
         :optionsStyle="() => ({ color: '#000000' })"
+        @click="showModal"
       />
       <l-geo-json
         v-if="showTitikKumpulMamungaaTimur"
         :geojson="titikKumpulMamungaaTimur"
-      />
+        @click="showModal"
+      >
+      </l-geo-json>
+      
 
       <l-geo-json
         v-if="showJalurEvakuasiPatoa"
         :geojson="jalurEvakuasiPatoa"
         :optionsStyle="() => ({ color: '#000000' })"
+        @click="showModal"
       />
-      <l-geo-json v-if="showTitikKumpulPatoa" :geojson="titikKumpulPatoa" />
+      <l-geo-json 
+      v-if="showTitikKumpulPatoa" 
+      :geojson="titikKumpulPatoa"
+      @click="showModal"
+       />
 
       <l-geo-json
         v-if="showJalurEvakuasiKaidundu"
         :geojson="jalurEvakuasiKaidundu"
         :optionsStyle="() => ({ color: '#000000' })"
+        @click="showModal"
       />
       <l-geo-json
         v-if="showTitikKumpulKaidundu"
         :geojson="titikKumpulKaidundu"
+        @click="showModal"
       />
 
       <l-geo-json
         v-if="showJalurEvakuasiBukitHijau"
         :geojson="jalurEvakuasiBukitHijau"
         :optionsStyle="() => ({ color: '#000000' })"
+        @click="showModal"
       />
       <l-geo-json
         v-if="showTitikKumpulBukitHijau"
         :geojson="titikKumpulBukitHijau"
+        @click="showModal"
       />
 
       <l-circle-marker v-if="currentLocation" :lat-lng="currentLocation">
@@ -266,4 +315,5 @@ export default {
       </l-circle-marker>
     </l-map>
   </div>
+  
 </template>
