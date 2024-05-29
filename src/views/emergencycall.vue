42420fda693ed4ac929a00a9e698d64d04bbf3ea
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2024-05-29 10:55:45 +0800

    fix: typo filepath kaidundu

--- src/views/emergencycall.vue
+++ src/views/emergencycall.vue
@@ -26,7 +26,15 @@ import { RouterLink } from "vue-router";
       </h2>
       <div class="w-full bg-slate-50">
         <Table class="font-medium">
-          <TableCaption></TableCaption>
+          <TableCaption>
+            Siaga Darurat di Kecamatan Bulawa: Pastikan Anda selalu siap
+            menghadapi segala situasi darurat dengan menyimpan nomor kontak
+            penting ini! Ketika bencana melanda atau situasi genting terjadi,
+            bantuan cepat dan tepat sangat dibutuhkan. Jangan panik, hubungi
+            layanan darurat yang siap siaga untuk memberikan pertolongan pertama
+            dan bantuan yang Anda perlukan. Keselamatan Anda adalah prioritas
+            kami!
+          </TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead class="w-[100px] font-bold">No</TableHead>
@@ -38,27 +46,50 @@ import { RouterLink } from "vue-router";
             <TableRow>
               <TableCell class="font-medium">01</TableCell>
               <TableCell>Bukit Hijau</TableCell>
-              <TableCell>083193459090</TableCell>
+              <TableCell>
+                <a class="text-yellow-600 underline" href="tel:082190054717">
+                  082190054717
+                </a>
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell class="font-medium">02</TableCell>
               <TableCell>Kaidundu</TableCell>
-              <TableCell>082199876543</TableCell>
+              <TableCell>
+                <a class="text-yellow-600 underline" href="tel:082211502256">
+                  082211502256
+                </a>
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell class="font-medium">03</TableCell>
               <TableCell>Patoa</TableCell>
-              <TableCell>085243321123</TableCell>
+              <TableCell>
+                <a class="text-yellow-600 underline" href="tel:081244263899">
+                  081244263899
+                </a>
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell class="font-medium">04</TableCell>
               <TableCell>Mamungaa Timur</TableCell>
-              <TableCell>085367588898</TableCell>
+              <TableCell>
+                <a class="text-yellow-600 underline" href="tel:081342581898">
+                  081342581898
+                </a>
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
       </div>
-      <Button :as="RouterLink" to="/">Kembali </Button>
+      <Button
+        class="flex items-center justify-center gap-1"
+        :as="RouterLink"
+        to="/"
+      >
+        <v-icon name="fa-arrow-left" />
+        Kembali
+      </Button>
     </div>
   </section>
 </template>
